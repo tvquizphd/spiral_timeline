1f7eea50dffc614d5428ae3c3c6baf051be1ba66
TV Quiz PhD <75504552+tvquizphd@users.noreply.github.com>
2022-10-08 10:27:03 -0400

    Update index.py

--- index.py
+++ index.py
@@ -9,46 +9,48 @@ import pandas as pd
 import math
 
 # styling
-LINEWIDTH=4
-EDGEWIDTH=1
+LINEWIDTH=6
+EDGEWIDTH=3
 CAPSTYLE="projecting"
 COLORMAP="viridis"
 ALPHA=1
 FIRSTDAY=6 # 0=Mon, 6=Sun
 MARGIN=4
 OUTLIER=3 #add outlier with 3x y limit
-N_RINGS = 60
+N_RINGS = 10
 PX_DIAMETER = 700 # rough pixels
 REAL_LIMIT = 1000
 MIN_RADIUS = LINEWIDTH + EDGEWIDTH
 PX_RADIUS = PX_DIAMETER / 2
+CORE_STOP = 1
 
 def to_spiral(n_rings, in_d, out_d):
     return n_rings * np.pi * (in_d + out_d) / 2
 
 def from_arc(arc, radius):
-    radians = arc / max(MIN_RADIUS, radius)
+    radians = arc / radius
     return radians / (2 * np.pi)
 
 def scale_1(n, h):
-    return math.log10(max(1,REAL_LIMIT * (n / h)))
+    return REAL_LIMIT * (n / h)
 
 def scale_2(n, px):
     return n * px
 
-def inverse_scales(n, px):
-    return 10**(n / px)
+def inverse_scale_2(n, px):
+    return n / px
 
 barchart = rgb2gray(rgba2rgb(io.imread("barchart.png")))
 out = barchart > threshold_otsu(barchart)
 
 deltas = [scale_1(np.count_nonzero(o), out.shape[0]) for o in np.transpose(out)[::2]]
-maximum = scale_1(limit*OUTLIER, out.shape[0])
+maximum = scale_1(out.shape[0]*OUTLIER, out.shape[0])
 deltas = ([maximum] + deltas)
 
 px_ratio = to_spiral(N_RINGS, MIN_RADIUS, PX_DIAMETER) / sum(deltas)
 arcs = [scale_2(d, px_ratio) for d in deltas]
-starts = [0]
+real_limit = max(arcs[1:])
+starts = [CORE_STOP]
 for arc in arcs:
     last_turn = starts[-1]
     last_rad = PX_DIAMETER * last_turn / N_RINGS
@@ -67,18 +69,24 @@ df["stop"] = starts[1:]
 fig = plt.figure(figsize=(16, 12))
 ax = fig.gca(projection="polar")
 
+core_nsamples = int(1000. * (CORE_STOP - 0))
+core_t = np.linspace(0, CORE_STOP, core_nsamples)
+core_theta = 2 * np.pi * core_t
+ax.plot(core_theta, core_t, lw=LINEWIDTH, color="white", solid_capstyle=CAPSTYLE, alpha=ALPHA)
+
 for idx, event in df.iterrows():
     # sample normalized distance from colormap
-    ndist = event['dist'] / df['dist'].max()
+    ndist = event['dist'] / real_limit
     color = plt.cm.get_cmap(COLORMAP)(ndist)
     tstart, tstop = event.loc[['start', 'stop']]
     # timestamps are in week fractions, 2pi is one week
-    nsamples = int(1000. * (tstop - tstart))
+    nsamples = int(10000. * (tstop - tstart))
     t = np.linspace(tstart, tstop, nsamples)
     theta = 2 * np.pi * t
     arc, = ax.plot(theta, t, lw=LINEWIDTH, color=color, solid_capstyle=CAPSTYLE, alpha=ALPHA)
     if EDGEWIDTH > 0:
         arc.set_path_effects([mpe.Stroke(linewidth=LINEWIDTH+EDGEWIDTH, foreground='white'), mpe.Normal()])
+
 
 # grid and labels
 ax.set_rticks([])
@@ -90,13 +98,13 @@ ax.axis("off")
 ax.grid(True)
 # setup a custom colorbar, everything's always a bit tricky with mpl colorbars
 vmin = df['dist'].min()
-vmax = df['dist'].max()
-norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+norm = mpl.colors.Normalize(vmin=vmin, vmax=real_limit)
 sm = plt.cm.ScalarMappable(cmap=COLORMAP, norm=norm)
 sm.set_array([])
 tick_font_size = 10
-ticks = np.linspace(vmin, vmax, 10)
-tick_labels = [f"{int(inverse_scales(t, px_ratio))}" for t in ticks]
+ticks = np.linspace(vmin, real_limit, 10)
+tick_labels = [f"{int(inverse_scale_2(t, px_ratio))}" for t in ticks]
+tick_labels[-1] += "+"
 cbar = plt.colorbar(
     sm, ticks=ticks, fraction=0.04, 
     aspect=60, pad=0.1, label="magnitude", ax=ax
